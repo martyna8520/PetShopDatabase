f564b0286b81285b12da8dd3d94ceecfd3a36ad1
Martyna <martynaaszyk@gmail.com>
2024-01-01 21:06:02 +0100

    add statement for dropping entire database

--- drop.sql
+++ drop.sql
@@ -8,9 +8,14 @@ DROP TABLE Packages;
 DROP TABLE Employees;
 DROP TABLE Products_ordered;
 DROP TABLE Offers;
-DROP TABLE Delivery_options;
 DROP TABLE Orders;
+DROP TABLE Delivery_options;
 DROP TABLE Addresses;
 DROP TABLE Customers;
 DROP TABLE Products;
 DROP TABLE Producers;
+
+USE master;
+ALTER DATABASE pet_shop_database SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+DROP DATABASE pet_shop_database;
+
