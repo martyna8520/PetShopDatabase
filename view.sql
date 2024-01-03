1c896fe42c6e76e44884b81608af9550ae2e9105
Martyna <martynaaszyk@gmail.com>
2024-01-04 00:29:17 +0100

    add view of outcome of deleted value

--- view.sql
+++ view.sql
@@ -6,15 +6,15 @@ WHERE product_id = 1;
 
 SELECT * FROM Products WHERE product_id = 1;
 
-SELECT * FROM Offers WHERE product_id = 1;
 
 SELECT * FROM Products WHERE product_name = 'Fish Tank Filter';
+SELECT * FROM Offers WHERE product_id = (SELECT product_id FROM Products WHERE product_name = 'Fish Tank Filter');
 
 DELETE FROM Products
 WHERE product_name = 'Fish Tank Filter';
 
 SELECT * FROM Products WHERE product_name = 'Fish Tank Filter';;
 SELECT * FROM Offers WHERE product_id = (SELECT product_id FROM Products WHERE product_name = 'Fish Tank Filter');
-SELECT * FROM Products_ordered WHERE offer_id IN (SELECT offer_id FROM Offers WHERE product_id = (SELECT product_id FROM Products WHERE product_name = 'Fish Tank Filter'));
+
 
 
